67dd5c79064f82584c4c29ce250753b719c33666
Salma-007 <salmaelallali11@gmail.com>
2024-11-08 22:10:19 +0100

    activation de changement des icones onclick

--- src/script.js
+++ src/script.js
@@ -19,42 +19,63 @@ let section_cv7 = document.querySelector(".section-cv7");
 let section_cv8 = document.querySelector(".section-cv8");
 let section_cv9 = document.querySelector(".section-cv9");
 
+// icons
 
 
+
+function changeIcon(sectionIndex) {
+
+    let icons = document.querySelectorAll('.progress-item i');
+    
+    if (icons[sectionIndex - 1]) {  
+        icons[sectionIndex - 1].classList.remove('fa-spinner');  
+        icons[sectionIndex - 1].classList.add('fa-check');
+    }
+}
+
+// les fonctions de change de section de form et barre de progress
 function nextSection2() {
         section_cv1.style.display = "none";
         section_cv2.style.display = "block";
-
+        changeIcon(1)
 }
 function nextSection3() {
     section_cv2.style.display = "none";
     section_cv3.style.display = "block";
+    changeIcon(2)
 }
 function nextSection4() {
     section_cv3.style.display = "none";
     section_cv4.style.display = "block";
+    changeIcon(3)
 }
 function nextSection5() {
     section_cv4.style.display = "none";
     section_cv5.style.display = "block";
+    changeIcon(4)
 }
 function nextSection6() {
     section_cv5.style.display = "none";
     section_cv6.style.display = "block";
+    changeIcon(5)
 }
 function nextSection7() {
     section_cv6.style.display = "none";
     section_cv7.style.display = "block";
+    changeIcon(6)
 }
 function nextSection8() {
     section_cv7.style.display = "none";
     section_cv8.style.display = "block";
+    changeIcon(7)
 }
 function nextSection9() {
     section_cv8.style.display = "none";
     section_cv9.style.display = "block";
+    changeIcon(8)
 }
 
+// onclick de chaque bouton
 if (boutonSuivant1) {
     boutonSuivant1.addEventListener("click", nextSection2);
 }
